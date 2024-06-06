885384ee8d0d37ae4d2579b176d103d0c92ecbf3
LENOVO <LENOVO@LAPTOP-P1U8KGS8>
2024-06-06 08:51:30 -0600

    agregar formulario de ingreso

--- Examen_Final/Laptop.cs
+++ Examen_Final/Laptop.cs
@@ -5,12 +5,12 @@ namespace Examen_Final
     public class Laptop:Dispositivo
     {
         public int Memoria_ram {  get; set; }
-        public int Capacidad { get; set; }
+        public string Capacidad { get; set; }
 
         public Laptop() 
         {
             Memoria_ram = 0;
-            Capacidad = 0;
+            Capacidad = string.Empty;
         }
     }
 }
